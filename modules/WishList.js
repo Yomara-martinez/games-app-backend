bb84a18c466a835e93138311a74beb63186077e5
Yomara <yomara024@outlook.com>
2026-08-02 13:36:50 -0400

    add new route

--- modules/WishList.js
+++ modules/WishList.js
@@ -2,15 +2,16 @@ const { DataTypes } = require("sequelize");
 const dbConnection = require("../db");
 
 const WishList = dbConnection.define("wishList", {
-    
+
   userId: {
     type: DataTypes.UUID,
     allowNull: false,
+    unique: true,
   },
   gameReviewId:{
 type: DataTypes.INTEGER,
 allowNull: false,
-
+unique: true,
   }
 }
 )
